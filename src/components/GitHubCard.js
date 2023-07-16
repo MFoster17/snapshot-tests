503edda6838ab6a38ab99585aca1236781eacb56
MFoster17 <ausslanderofficial@gmail.com>
2023-07-16 02:38:13 -0700

    finalized all files

---
+++ src/components/GitHubCard.js
@@ -0,0 +1,14 @@
+import Card from 'react-bootstrap/Card';
+
+function GitHubCard() {
+  return (
+    <Card>
+      <Card.Body>Username: Mfoster17
+                img: src/Images/IMG_3263 Small.png
+                Im a passionate Laker fan enjoying learning react
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default GitHubCard;
